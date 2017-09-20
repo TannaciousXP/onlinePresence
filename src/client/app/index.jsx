c150632e2b6ce704745cda833ea14662f0e6a0b3
Peter Tan <pxhtan@gmail.com>
2017-09-19 22:13:32 -0700

    Add React

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,1 +1,18 @@
-console.log('Hello World');
+import React from 'react';
+import {render} from 'react-dom';
+
+import AwesomeComponent from './AwesomeComponent.jsx';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <p> Hello React!</p>
+        <AwesomeComponent />
+      </div>
+      
+    );
+  }
+}
+
+render(<App/>, document.getElementById('app'));
