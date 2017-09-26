5e1e4d644299a5762662ddf78ba4948889f18d77
Peter Tan <pxhtan@gmail.com>
2017-09-25 20:03:40 -0700

    Reorganize folders

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
+var SRC_DIR = path.resolve(__dirname, 'client/src');
+var DIST_DIR = path.resolve(__dirname, 'client/dist/public');
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
-    path: BUILD_DIR,
+    path: DIST_DIR,
     filename: 'bundle.js'
   },
   module : {
     loaders : [
       {
         test : /\.jsx?/,
-        include : APP_DIR,
+        include : SRC_DIR,
         loader : 'babel-loader'
       }
     ]
