6c5e87bc440aa5b7c300711d38aaa5ef058a4c99
Peter Tan <pxhtan@gmail.com>
2017-10-02 16:59:44 -0700

    create ejs

--- server/app.js
+++ server/app.js
@@ -2,8 +2,17 @@
 const express = require('express');
 const path = require('path');
 // const routes = require('./routes');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(expres.static(__dirname + '/../client/dist/public'));
+
 
 
 module.exports = app;
