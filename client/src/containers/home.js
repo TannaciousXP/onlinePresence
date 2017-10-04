c18b182fa7f0c809a7d63bbacb0609765306ca27
Peter Tan <pxhtan@gmail.com>
2017-10-04 13:55:10 -0700

    create home page

---
+++ client/src/containers/home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return (
+      <div>Hello PXHTAN!</div>
+    );
+  }
+}
+
+export default Home;
