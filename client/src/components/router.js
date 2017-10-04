66f0a1313b50fabf04c8044b9d72efaaf76ea926
Peter Tan <pxhtan@gmail.com>
2017-10-04 13:54:41 -0700

    create router between pages

---
+++ client/src/components/router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from '../containers/home';
+
+const Router = () => (
+  <Switch>
+    <Route exact path="/">
+      <Home />
+    </Route>
+  </Switch>
+);
