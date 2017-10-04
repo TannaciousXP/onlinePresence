38491bae6ef8c67eb8797232e093da7d426d889f
Peter Tan <pxhtan@gmail.com>
2017-10-04 13:54:07 -0700

    add redux and router

--- client/src/app.js
+++ client/src/app.js
@@ -1,18 +1,27 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 
-import AwesomeComponent from './components/AwesomeComponent';
+import { BrowserRouter } from 'react-router-dom';
 
-class App extends Component {
-  render () {
-    return (
-      <div>
-        <p> Hello React!</p>
-        <AwesomeComponent />
-      </div>
-      
-    );
-  }
-}
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import promise from 'redux-promise';
+import reducers from './reducers';
+
+import Router from './components/router';
+
+const enhancers = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+const App = () => (
+  <Provider store={createStoreWithMiddleware(reducers, {}, enhancers)}>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </Provider>
+);
 
 render(<App/>, document.getElementById('app'));
