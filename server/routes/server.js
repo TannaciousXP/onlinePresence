74fc3020ae491b2093fe21365f743b3bca9e9855
Peter Tan <pxhtan@gmail.com>
2017-10-11 16:42:26 -0700

    Render with title infomation

--- server/routes/server.js
+++ server/routes/server.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const $ = require('jquery');
 
+// Render home page
 router.route('/')
   .get((req, res) => {
-    res.render('pages/home.ejs');
+    res.render('pages/home.ejs', { title: 'Home' });
   });
 
+// Render about page
 router.route('/about')
   .get((req, res) => {
-    res.render('pages/about.ejs');
+    res.render('pages/about.ejs', { title: 'About' });
   });
 
+// Render projects page
 router.route('/projects')
   .get((req, res) => {
-    res.render('pages/projects.ejs');
+    res.render('pages/projects.ejs', { title: 'Project' });
   });
 
 module.exports = router;
