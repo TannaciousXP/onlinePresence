cbedcd99729c101a3ff6bf430a6d752e75fbdf53
Peter Tan <pxhtan@gmail.com>
2017-10-12 14:30:11 -0700

    react component preloader

---
+++ client/src/components/Preloader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Preloader = (props) => (
+  <div id="preloader">
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+)
+
+
+export default Preloader;
