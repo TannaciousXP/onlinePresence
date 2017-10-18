59f1ba955f6301d088086bd040ca8b618a306a7d
Peter Tan <pxhtan@gmail.com>
2017-10-18 15:46:32 -0700

    add style to reveal component

--- client/src/containers/home.js
+++ client/src/containers/home.js
@@ -11,20 +11,29 @@ export default class Home extends Component {
 
   render() {
     return (
-      <Grid container celled='internally' verticalAlign='middle' centered>
+      <Grid container celled='internally' verticalAlign='middle' centered padded>
         <Grid.Row stretched>
           <Segment raised textAlign='left'>
             <Grid columns={2} divided verticalAlign='middle'>
               <Grid.Column width={5}>
-                <Image size='medium' shape='circular' centered={true} src='/assets/photos/Photoshoot.jpg'/>
+                <Image size='medium' shape='circular' src='/assets/photos/Photoshoot.jpg'/>
               </Grid.Column>
 
               <Grid.Column width={11}>
-                  <Header as='h1'>PERSONAL NARRATIVE</Header>
-                  <Header.Content size='medium'>
-                  I am a Javascript engineer with experience across the stack and a strong command of relevant technologies. Recently, I built a communication platform for hobbyists with the major features such as real-time messaging through Socket.IO, web-conferences with WebRTC, group events utilizing Google Maps API and OpenWeatherMap API to enhance the visualization of the event’s detail. The back-end was constructed with PostgreSQL for the aggregation of polymorphic data, and I utilized Knex together with Bookshelf to simplify SQL queries and facilitate seamless alterations to the database schema. As for the front-end, I modularized the components with React for a smooth and intuitive user interface and managed the state with Redux for e cient data  ow and improved maintainability.
-                  </Header.Content>
-                </Grid.Column>
+                <Header as='h1'>PERSONAL NARRATIVE</Header>
+                <Header.Content size='medium'>
+                I am a Javascript engineer with experience across the stack and a strong \
+                command of relevant technologies. Recently, I built a communication platform \
+                for hobbyists with the major features such as real-time messaging through \
+                Socket.IO, web-conferences with WebRTC, group events utilizing Google Maps \
+                API and OpenWeatherMap API to enhance the visualization of the event’s detail. \
+                The back-end was constructed with PostgreSQL for the aggregation of polymorphic \
+                data, and I utilized Knex together with Bookshelf to simplify SQL queries and \
+                facilitate seamless alterations to the database schema. As for the front-end, I \
+                modularized the components with React for a smooth and intuitive user interface \
+                and managed the state with Redux for e cient data  ow and improved maintainability.
+                </Header.Content>
+              </Grid.Column>
             </Grid>
           </Segment>
         </Grid.Row>
@@ -33,34 +42,59 @@ export default class Home extends Component {
           <Grid.Column>
             <Reveal animated='move down'>
               <Reveal.Content visible>
-                <Image shape='circular' size='small' src='/assets/logogs/.png'></Image>
+                <Image
+                  size='small'
+                  shape='circular'
+                  src='/assets/logos/Learning.png'
+                  centered
+                />
+              </Reveal.Content>
+              <Reveal.Content hidden>
+                <Image
+                  size='small'
+                  shape='circular'
+                  src='/assets/photos/Photoshoot.jpg'
+                  centered
+                />
+              </Reveal.Content>
+            </Reveal>
+          </Grid.Column>
+          <Grid.Column>
+            <Reveal animated='move down'>
+              <Reveal.Content visible>
+                <Image
+                  shape='circular'
+                  size='small'
+                  centered
+                  src='/assets/logos/Pets.png'
+                />
+
+              </Reveal.Content>
+              <Reveal.Content hidden>
+                <Image
+                  size='small'
+                  shape='circular'
+                  src='/assets/photos/Photoshoot.jpg'
+                  centered
+                />
+              </Reveal.Content>
+            </Reveal>
+          </Grid.Column>
+          <Grid.Column>
+            <Reveal animated='move down'>
+              <Reveal.Content visible>
+                <Image
+                  shape='circular'
+                  size='small'
+                  centered
+                  src='/assets/logos/Fitness.png'
+                />
+
               </Reveal.Content>
               <Reveal.Content hidden>
 
               </Reveal.Content>
             </Reveal>
-          </Grid.Column>
-          <Grid.Column>
-            <Reveal animated='move down'>
-            <Reveal.Content visible>
-              <Image shape='circular' size='small' src='/assets/logogs/.png'></Image>
-
-            </Reveal.Content>
-            <Reveal.Content hidden>
-
-            </Reveal.Content>
-            </Reveal>
-          </Grid.Column>
-          <Grid.Column>
-            <Reveal animated='move down'>
-            <Reveal.Content visible>
-              <Image shape='circular' size='small' src='/assets/logogs/.png'></Image>
-
-            </Reveal.Content>
-            <Reveal.Content hidden>
-
-            </Reveal.Content>
-          </Reveal>
           </Grid.Column>
         </Grid.Row>
       </Grid>
