31ed5b2077f11acec11551269942db35b35bd939
Peter Tan <pxhtan@gmail.com>
2017-10-18 13:49:06 -0700

    add more logos for reveal

--- client/src/containers/home.js
+++ client/src/containers/home.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import { Grid, Image, Header, Segment } from 'semantic-ui-react';
+import { Grid, Image, Header, Segment, Reveal } from 'semantic-ui-react';
 
 export default class Home extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       isLoading: true
@@ -11,26 +11,57 @@ export default class Home extends Component {
 
   render() {
     return (
-      <Grid container celled='internally' verticalAlign='middle' centered={true}> 
+      <Grid container celled='internally' verticalAlign='middle' centered>
         <Grid.Row stretched>
           <Segment raised textAlign='left'>
             <Grid columns={2} divided verticalAlign='middle'>
               <Grid.Column width={5}>
-                <Image size='medium' shape='circular' centered={true} src='/assets/photos/Photoshoot.jpg'/>            
+                <Image size='medium' shape='circular' centered={true} src='/assets/photos/Photoshoot.jpg'/>
               </Grid.Column>
 
               <Grid.Column width={11}>
-                  <Header as='h2'>PERSONAL NARRATIVE</Header>
-                  <Header.Content>
+                  <Header as='h1'>PERSONAL NARRATIVE</Header>
+                  <Header.Content size='medium'>
                   I am a Javascript engineer with experience across the stack and a strong command of relevant technologies. Recently, I built a communication platform for hobbyists with the major features such as real-time messaging through Socket.IO, web-conferences with WebRTC, group events utilizing Google Maps API and OpenWeatherMap API to enhance the visualization of the event’s detail. The back-end was constructed with PostgreSQL for the aggregation of polymorphic data, and I utilized Knex together with Bookshelf to simplify SQL queries and facilitate seamless alterations to the database schema. As for the front-end, I modularized the components with React for a smooth and intuitive user interface and managed the state with Redux for e cient data  ow and improved maintainability.
                   </Header.Content>
-                </Grid.Column>            
-            </Grid>          
+                </Grid.Column>
+            </Grid>
           </Segment>
         </Grid.Row>
 
-        <Grid.Row>
+        <Grid.Row columns={3}>
+          <Grid.Column>
+            <Reveal animated='move down'>
+              <Reveal.Content visible>
+                <Image shape='circular' size='small' src='/assets/logogs/.png'></Image>
+              </Reveal.Content>
+              <Reveal.Content hidden>
 
+              </Reveal.Content>
+            </Reveal>
+          </Grid.Column>
+          <Grid.Column>
+            <Reveal animated='move down'>
+            <Reveal.Content visible>
+              <Image shape='circular' size='small' src='/assets/logogs/.png'></Image>
+
+            </Reveal.Content>
+            <Reveal.Content hidden>
+
+            </Reveal.Content>
+            </Reveal>
+          </Grid.Column>
+          <Grid.Column>
+            <Reveal animated='move down'>
+            <Reveal.Content visible>
+              <Image shape='circular' size='small' src='/assets/logogs/.png'></Image>
+
+            </Reveal.Content>
+            <Reveal.Content hidden>
+
+            </Reveal.Content>
+          </Reveal>
+          </Grid.Column>
         </Grid.Row>
       </Grid>
     );
