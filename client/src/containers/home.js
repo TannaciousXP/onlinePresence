0ad531a3c6f46b340fcf7d8d2d02f88db31483d8
Peter Tan <pxhtan@gmail.com>
2017-10-17 20:46:43 -0700

    separate assets into sub folders, and foot ejs was refactor

--- client/src/containers/home.js
+++ client/src/containers/home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Header } from 'semantic-ui-react';
 
 export default class Home extends Component {
   constructor(props){
@@ -11,8 +11,21 @@ export default class Home extends Component {
 
   render() {
     return (
-      <div>Hello PXHTAN! REACT!
-      </div>
+      <Grid container celled='internally' verticalAlign='middle' centered stretched> 
+        <Grid.Row>
+        <Grid.Column width={16}>
+          <Image size='medium' shape='circular' src='/assets/'/>
+            <Header as='h2'>PERSONAL NARATIVE</Header>
+            <p>
+            I am a Javascript engineer with experience across the stack and a strong command of relevant technologies. Recently, I built a communication platform for hobbyists with the major features such as real-time messaging through Socket.IO, web-conferences with WebRTC, group events utilizing Google Maps API and OpenWeatherMap API to enhance the visualization of the event’s detail. The back-end was constructed with PostgreSQL for the aggregation of polymorphic data, and I utilized Knex together with Bookshelf to simplify SQL queries and facilitate seamless alterations to the database schema. As for the front-end, I modularized the components with React for a smooth and intuitive user interface and managed the state with Redux for e cient data  ow and improved maintainability.
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+
+        </Grid.Row>
+      </Grid>
     );
   }
 }
