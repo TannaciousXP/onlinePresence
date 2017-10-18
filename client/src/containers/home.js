cf0f7d36714db7e9f666cf3fe44d67ac1870f4da
Peter Tan <pxhtan@gmail.com>
2017-10-17 20:57:10 -0700

    Add self photo

--- client/src/containers/home.js
+++ client/src/containers/home.js
@@ -14,7 +14,7 @@ export default class Home extends Component {
       <Grid container celled='internally' verticalAlign='middle' centered stretched> 
         <Grid.Row>
         <Grid.Column width={16}>
-          <Image size='medium' shape='circular' src='/assets/'/>
+          <Image size='medium' shape='circular' src='/assets/photos/Photoshoot.jpg' floated='left'/>
             <Header as='h2'>PERSONAL NARATIVE</Header>
             <p>
             I am a Javascript engineer with experience across the stack and a strong command of relevant technologies. Recently, I built a communication platform for hobbyists with the major features such as real-time messaging through Socket.IO, web-conferences with WebRTC, group events utilizing Google Maps API and OpenWeatherMap API to enhance the visualization of the event’s detail. The back-end was constructed with PostgreSQL for the aggregation of polymorphic data, and I utilized Knex together with Bookshelf to simplify SQL queries and facilitate seamless alterations to the database schema. As for the front-end, I modularized the components with React for a smooth and intuitive user interface and managed the state with Redux for e cient data  ow and improved maintainability.
