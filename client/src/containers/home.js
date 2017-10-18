b582968adc69c9caecc9daeeb1c71812ee9ed853
Peter Tan <pxhtan@gmail.com>
2017-10-17 22:53:40 -0700

    Refactor foot.ejs

--- client/src/containers/home.js
+++ client/src/containers/home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Grid, Image, Header } from 'semantic-ui-react';
+import { Grid, Image, Header, Segment } from 'semantic-ui-react';
 
 export default class Home extends Component {
   constructor(props){
@@ -11,15 +11,22 @@ export default class Home extends Component {
 
   render() {
     return (
-      <Grid container celled='internally' verticalAlign='middle' centered stretched> 
-        <Grid.Row>
-        <Grid.Column width={16}>
-          <Image size='medium' shape='circular' src='/assets/photos/Photoshoot.jpg' floated='left'/>
-            <Header as='h2'>PERSONAL NARATIVE</Header>
-            <p>
-            I am a Javascript engineer with experience across the stack and a strong command of relevant technologies. Recently, I built a communication platform for hobbyists with the major features such as real-time messaging through Socket.IO, web-conferences with WebRTC, group events utilizing Google Maps API and OpenWeatherMap API to enhance the visualization of the event’s detail. The back-end was constructed with PostgreSQL for the aggregation of polymorphic data, and I utilized Knex together with Bookshelf to simplify SQL queries and facilitate seamless alterations to the database schema. As for the front-end, I modularized the components with React for a smooth and intuitive user interface and managed the state with Redux for e cient data  ow and improved maintainability.
-            </p>
-          </Grid.Column>
+      <Grid container celled='internally' verticalAlign='middle' centered={true}> 
+        <Grid.Row stretched>
+          <Segment raised textAlign='left'>
+            <Grid columns={2} divided verticalAlign='middle'>
+              <Grid.Column width={5}>
+                <Image size='medium' shape='circular' centered={true} src='/assets/photos/Photoshoot.jpg'/>            
+              </Grid.Column>
+
+              <Grid.Column width={11}>
+                  <Header as='h2'>PERSONAL NARATIVE</Header>
+                  <Header.Content>
+                  I am a Javascript engineer with experience across the stack and a strong command of relevant technologies. Recently, I built a communication platform for hobbyists with the major features such as real-time messaging through Socket.IO, web-conferences with WebRTC, group events utilizing Google Maps API and OpenWeatherMap API to enhance the visualization of the event’s detail. The back-end was constructed with PostgreSQL for the aggregation of polymorphic data, and I utilized Knex together with Bookshelf to simplify SQL queries and facilitate seamless alterations to the database schema. As for the front-end, I modularized the components with React for a smooth and intuitive user interface and managed the state with Redux for e cient data  ow and improved maintainability.
+                  </Header.Content>
+                </Grid.Column>            
+            </Grid>          
+          </Segment>
         </Grid.Row>
 
         <Grid.Row>
