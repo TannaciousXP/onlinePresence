febf0fd659510af83595386f46846b1509a3d659
Peter Tan <pxhtan@gmail.com>
2017-10-20 20:11:42 -0700

    finish blog with coming soon

--- client/src/containers/blogs.js
+++ client/src/containers/blogs.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import {
+  Header
+} from 'semantic-ui-react';
 
 export default class Blogs extends Component {
   render() {
     return (
-      <div>
-        Blogs
-      </div>
+      <Header as='h1' className='blog'>COMING SOON</Header>
     );
   }
 }
