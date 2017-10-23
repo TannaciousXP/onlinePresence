a757d936eaf1142f33c18744793d6313cd4ec49d
Peter Tan <pxhtan@gmail.com>
2017-10-22 22:13:49 -0700

    create controllers for projects

--- client/src/containers/about.js
+++ client/src/containers/about.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
 import {
-  Grid
+  Grid,
+  Header,
+  Segment,
+  Divider,
+  Reveal,
+  Button,
+  Image
 } from 'semantic-ui-react';
 
 export default class About extends Component {
@@ -8,7 +14,82 @@ export default class About extends Component {
     return (
       <Grid container verticalAlign='middle' centered padded>
         <Grid.Row>
-          <div>ABOUT</div>
+          <Segment raised textAlign='left'>
+            <Grid columns={2} verticalAlign='middle'>
+              <Grid.Column width={5}>
+                <Image size='medium' shape='circular' src='/assets/photos/photoshoot.jpg'/>
+              </Grid.Column>
+
+              <Grid.Column width={11}>
+                <Header className='head intro' as='h1'>SOFTWARE ENGINEER</Header>
+                <Divider fitted/>
+                <Header className='intro content' size='small' textAlign='justified'>
+            I am a Javascript engineer with experience across the stack and a strong
+            command of relevant technologies. Recently, I built a communication platform
+            for hobbyists with the major features such as real-time messaging through
+            Socket.IO, web-conferences with WebRTC, group events utilizing Google Maps
+            API and OpenWeatherMap API to enhance the visualization of the event’s detail.
+            The back-end was constructed with PostgreSQL for the aggregation of polymorphic
+            data, and I utilized Knex together with Bookshelf to simplify SQL queries and
+            facilitate seamless alterations to the database schema. As for the front-end, I
+            modularized the components with React for a smooth and intuitive user interface
+            and managed the state with Redux for e cient data  ow and improved maintainability.
+                </Header>
+              </Grid.Column>
+            </Grid>
+          </Segment>
+        </Grid.Row>
+        <Grid.Row>
+          <Segment raised textAlign='left'>
+            <Grid columns={2} verticalAlign='middle'>
+
+              <Grid.Column width={11}>
+                <Header className='head intro' as='h1'>CUTS & GLORY CO</Header>
+                <Divider fitted/>
+                <Header className='intro content' size='small' textAlign='justified'>
+            I am a Javascript engineer with experience across the stack and a strong
+            command of relevant technologies. Recently, I built a communication platform
+            for hobbyists with the major features such as real-time messaging through
+            Socket.IO, web-conferences with WebRTC, group events utilizing Google Maps
+            API and OpenWeatherMap API to enhance the visualization of the event’s detail.
+            The back-end was constructed with PostgreSQL for the aggregation of polymorphic
+            data, and I utilized Knex together with Bookshelf to simplify SQL queries and
+            facilitate seamless alterations to the database schema. As for the front-end, I
+            modularized the components with React for a smooth and intuitive user interface
+            and managed the state with Redux for e cient data  ow and improved maintainability.
+                </Header>
+              </Grid.Column>
+              <Grid.Column width={5}>
+                <Image size='medium' shape='circular' src='/assets/photos/photoshoot.jpg'/>
+              </Grid.Column>
+            </Grid>
+          </Segment>
+        </Grid.Row>
+        <Grid.Row>
+          <Segment raised textAlign='left'>
+            <Grid columns={2} verticalAlign='middle'>
+              <Grid.Column width={5}>
+                <Image size='medium' shape='circular' src='/assets/photos/photoshoot.jpg'/>
+              </Grid.Column>
+
+              <Grid.Column width={11}>
+                <Header className='head intro' as='h1'>VELVETY & BLACK BEAR</Header>
+                <Divider fitted/>
+                <Header className='intro content' size='small' textAlign='justified'>
+            I am a Javascript engineer with experience across the stack and a strong
+            command of relevant technologies. Recently, I built a communication platform
+            for hobbyists with the major features such as real-time messaging through
+            Socket.IO, web-conferences with WebRTC, group events utilizing Google Maps
+            API and OpenWeatherMap API to enhance the visualization of the event’s detail.
+            The back-end was constructed with PostgreSQL for the aggregation of polymorphic
+            data, and I utilized Knex together with Bookshelf to simplify SQL queries and
+            facilitate seamless alterations to the database schema. As for the front-end, I
+            modularized the components with React for a smooth and intuitive user interface
+            and managed the state with Redux for e cient data  ow and improved maintainability.
+                </Header>
+              </Grid.Column>
+            </Grid>
+          </Segment>
         </Grid.Row>
       </Grid>
     );
