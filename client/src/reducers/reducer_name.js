0340a74378b18643da3e517480fa2838687f36c1
Peter Tan <pxhtan@gmail.com>
2017-10-23 11:01:33 -0700

    refactor spacing

--- client/src/reducers/reducer_name.js
+++ client/src/reducers/reducer_name.js
@@ -1,10 +1,10 @@
 import { FETCH_NAME } from '../actions';
 
 export default function(state = {}, action) {
-  switch(action.type) {
-    case FETCH_NAME:
-      return action.payload;
-    default:
-      return state;
+  switch (action.type) {
+  case FETCH_NAME:
+    return action.payload;
+  default:
+    return state;
   }
 }
