c1ec7b42132beb8de6991673e4d1b55c94db28b5
Peter Tan <pxhtan@gmail.com>
2017-10-23 11:01:10 -0700

    create boolean for fetch repo

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,30 @@
 import axios from 'axios';
 
 export const FETCH_NAME = 'fetch_name';
+export const IS_REPO_FETCHED = 'is_repo_fetched';
+export const REPO_FETCHED = 'repo_fetched';
 
-export let fetchName = function(a) {
+
+// Return userName for Github repo
+export let fetchName = function() {
   return {
     type: FETCH_NAME,
-    payload: 'Peter X. Tan'
+    payload: 'TannaciousXP'
+  };
+};
+
+// Return false for isRepoFetched
+export let isRepoFetched = function() {
+  return {
+    type: IS_REPO_FETCHED,
+    payload: false
+  };
+};
+
+// Return true for repoFetched
+export let repoFetched = function() {
+  return {
+    type: REPO_FETCHED,
+    payload: true,
   };
 };
