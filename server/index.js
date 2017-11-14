e270501ec356e04255b190ebf1d35afd97dc846c
Peter Tan <pxhtan@gmail.com>
2017-11-14 14:53:37 -0800

    change port back to 3000

--- server/index.js
+++ server/index.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const PORT = process.env.port || 3000;
 
 app.listen(PORT, () => {
-  console.log('PXTAN page listening on port 3000!');
+  console.log(`PXTAN page listening on port ${PORT}!`);
 });
