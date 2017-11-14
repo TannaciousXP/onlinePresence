ec3caf8232991f4dff5ba8be2e3744291405b665
Peter Tan <pxhtan@gmail.com>
2017-11-14 14:27:20 -0800

    add nohup.out to gitignore and change port to 80

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 'use strict';
 const app = require('./app');
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 80;
 
-app.listen(PORT, () => {
+app.listen(80, () => {
   console.log('PXTAN page listening on port 3000!');
 });
