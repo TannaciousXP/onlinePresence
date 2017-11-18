eabc3cec34db3d995eb4b9e16296c634a6c7b487
Peter Tan <pxhtan@gmail.com>
2017-11-17 17:13:19 -0800

    add modal for home artifacts

---
+++ client/src/components/modals/home_artifacts.js
@@ -0,0 +1,20 @@
+import React, { Comopnent } from 'react';
+
+import {
+  Modal
+} from 'semantic-ui-react';
+
+
+
+class Artifacts extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>testing</div>
+    );
+  }
+
+}
