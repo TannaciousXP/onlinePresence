17f1a9d9efbf2b5325411b9316567faec11e6163
Peter Tan <pxhtan@gmail.com>
2017-11-19 16:05:58 -0800

    add pictures for organize aspect

--- client/src/components/modals/home_artifacts.js
+++ client/src/components/modals/home_artifacts.js
@@ -27,33 +27,47 @@ class Artifacts extends Component {
         open={(isModalOpen && 'Artifact' === renderModal) ? true : false}
         dimmer='blurring'
       >
-        <Modal.Header>ARTIFACTS</Modal.Header>
+        <Modal.Header>ARTIFACTS: <span style={{color: 'rgb(236, 240, 235)'}}>click on the <span className='subLinks'> red </span> words to view code snippet</span></Modal.Header>
         <Modal.Content>
           <Modal.Description>
 
             <Header size='medium'>I write clean code in variety of languages and frameworks:</Header>
-            <li><a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.kSchemas)}><strong>SQL schemas with Knex</strong></a></li>
-            <li><a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.bsModel)}><strong>SQL models with Bookshelf ORM</strong></a></li>
-            <li><a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.kQuery)}><strong>SQL query builder with Knex</strong></a></li>
-            <li><a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.kMigration)}><strong>SQL schemas migration Knex</strong></a></li>
-            <li><a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.cJavaScript)}><strong>Client-side JavaScript</strong></a></li>
-            <li><a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.modHTMLCSS)}><strong>Modern HTML and CSS</strong></a></li>
-            <br/>
+            <List as='ul'>
+              <List.Item as ='li'>
+                <a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.kSchemas)}><strong>SQL schemas with Knex</strong></a>
+              </List.Item>
+              <List.Item as ='li'>
+                <a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.bsModel)}><strong>SQL models with Bookshelf ORM</strong></a>
+              </List.Item>
+              <List.Item as ='li'>
+                <a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.kQuery)}><strong>SQL query builder with Knex</strong></a>
+              </List.Item>
+              <List.Item as ='li'>
+                <a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.kMigration)}><strong>SQL schemas migration Knex</strong></a>
+              </List.Item>
+              <List.Item as ='li'>
+                <a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.cJavaScript)}><strong>Client-side JavaScript</strong></a>
+              </List.Item>
+              <List.Item as ='li'>
+                <a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.modHTMLCSS)}><strong>Modern HTML and CSS</strong></a>
+              </List.Item>
+            </List>
+
 
             <strong>REACT Framework: <br/></strong>
             <List as='ol'>
               <List.Item as='li' value='•'><a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.reactComp)}><strong>React component</strong></a></List.Item>
               <List.Item as='li' value='•'><strong>Redux:</strong>
                 <List.List as='ol'>
-                  <List.Item as='li' value='-'><a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.reactComp)}><strong>Redux Action</strong></a></List.Item>
-                  <List.Item as='li' value='-'><a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.reduxReducer)}><strong>Redux Reducer</strong></a></List.Item>
-                  <List.Item as='li' value='-'><a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.rootReducer)}><strong>Root Reducer</strong></a></List.Item>
+                  <List.Item as='li' value='°'><a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.reactComp)}><strong>Redux Action</strong></a></List.Item>
+                  <List.Item as='li' value='°'><a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.reduxReducer)}><strong>Redux Reducer</strong></a></List.Item>
+                  <List.Item as='li' value='°'><a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.rootReducer)}><strong>Root Reducer</strong></a></List.Item>
                 </List.List>
               </List.Item>
               <List.Item as='li' value='•'><strong>React Router</strong>
                 <List.List as='ol'>
-                  <List.Item as='li' value='-'><a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.reactRouterDom)}><strong>React-Router-Dom</strong></a></List.Item>
-                  <List.Item as='li' value='-'><a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.browserRouter)}><strong>Browser Router</strong></a></List.Item>
+                  <List.Item as='li' value='°'><a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.reactRouterDom)}><strong>React°Router°Dom</strong></a></List.Item>
+                  <List.Item as='li' value='°'><a className='subLinks' onClick={() => helper.openNewTab(links.artifacts.browserRouter)}><strong>Browser Router</strong></a></List.Item>
                 </List.List>
               </List.Item>
             </List>
@@ -63,6 +77,17 @@ class Artifacts extends Component {
               making things concrete, is important to getting a working plan of attack.
               </Header.Subheader>
             </Header>
+            <strong>Omni Chat planning:</strong>
+            <List as='ol'>
+              <List.Item as='li' value='•'>
+              Structured, step-by-step problem solving process road map
+                <List.Item as='ol'>
+                  <List.Item as='li' value='°'>
+                    <Image size='medium' src='/assets/artifacts/roadMap.png'/>
+                  </List.Item>
+                </List.Item>
+              </List.Item>
+            </List>
 
           </Modal.Description>
 
